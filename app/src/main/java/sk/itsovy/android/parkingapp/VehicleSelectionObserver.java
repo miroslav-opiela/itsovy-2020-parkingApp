b8fbfcc7f043ab49af72ef326bb0e4a22e97cc41
miroslav-opiela <miroslav-opiela@upjs.sk>
2021-01-22 10:53:04 +0100

    action mode

--- app/src/main/java/sk/itsovy/android/parkingapp/VehicleSelectionObserver.java
+++ app/src/main/java/sk/itsovy/android/parkingapp/VehicleSelectionObserver.java
@@ -1,14 +1,19 @@
 package sk.itsovy.android.parkingapp;
 
 import android.app.AppComponentFactory;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ActionMode;
 import androidx.recyclerview.selection.SelectionTracker;
 
-public class VehicleSelectionObserver extends SelectionTracker.SelectionObserver<Long> {
+public class VehicleSelectionObserver extends SelectionTracker.SelectionObserver<Long> implements ActionMode.Callback {
 
     private AppCompatActivity activity;
     private SelectionTracker<Long> selectionTracker;
+
+    private ActionMode actionMode;
 
     public VehicleSelectionObserver(AppCompatActivity activity,
                                     SelectionTracker<Long> selectionTracker) {
@@ -19,9 +24,34 @@ public class VehicleSelectionObserver extends SelectionTracker.SelectionObserver
     @Override
     public void onSelectionChanged() {
         if (selectionTracker.hasSelection()) {
-            activity.setTitle("Selected: " + selectionTracker.getSelection().size());
-        } else {
-            activity.setTitle(R.string.app_name);
+            // ak ide o prvy vyber
+            if (actionMode == null) {
+                actionMode = activity.startSupportActionMode(this);
+            }
+            actionMode.setTitle("Selected: " + selectionTracker.getSelection().size());
         }
+    }
+
+    @Override
+    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+        // tu pridame ikonku na vymazanie
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+        return false;
+    }
+
+    @Override
+    public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+        // tu vyriesime kliknutie na ikonku a vymazeme vozidla
+        return false;
+    }
+
+    @Override
+    public void onDestroyActionMode(ActionMode mode) {
+        selectionTracker.clearSelection();
+        actionMode = null;
     }
 }
